bd04d890665f353fb9b1f0f597f1ef7d60e26f78
Sai Prasad Patro <saipatra2012@gmail.com>
2022-03-03 00:37:05 +0530

    Mono repo initial commit

---
+++ apps/remote/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+import RemoteEntry from './components/RemoteEntry';
+
+export function App() {
+  return (
+    <>
+      <RemoteEntry />
+      <div />
+    </>
+  );
+}
+
+export default App;
---
+++ apps/shell/src/app/app.tsx
@@ -0,0 +1,9 @@
+import RemoteEntry from 'remote/RemoteEntry';
+
+export function App() {
+  return (
+      <RemoteEntry />
+  );
+}
+
+export default App;
